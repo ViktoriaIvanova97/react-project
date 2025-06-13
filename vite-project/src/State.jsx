18f6c5b44e81ae65931e7fde3e515f62910f50a9
Ivanova Viktoria <ivanovaviktoria97@yandex.by>
2025-06-13 15:18:42 +0300

    add hidden text

--- vite-project/src/State.jsx
+++ vite-project/src/State.jsx
@@ -2,11 +2,27 @@ import React, { useState } from "react";
 
 const StateFun = () => {
   const [increaseState, increaseSetState] = useState(0);
+  const [hideState, hideSetState] = useState(false);
 
   return (
     <>
-      <p>state: {increaseState}</p>
-      <button onClick={() => increaseSetState(preState =>preState + 1)}>click</button>
+      <div>
+        <p>state: {increaseState}</p>
+        <button onClick={() => increaseSetState((preState) => preState + 1)}>
+          click
+        </button>
+      </div>
+
+      <div>
+        {hideState && <p>HELLO!</p>}
+        <button
+          onClick={() => {
+            hideSetState((hide) => !hide);
+          }}
+        >
+          Показать/скрыть текст
+        </button>
+      </div>
     </>
   );
 };
