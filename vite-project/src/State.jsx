af5f5f09e524d2bcf0356e904c4cbff42a79d7e4
Ivanova Viktoria <ivanovaviktoria97@yandex.by>
2025-06-13 15:39:56 +0300

    add input

--- vite-project/src/State.jsx
+++ vite-project/src/State.jsx
@@ -3,6 +3,11 @@ import React, { useState } from "react";
 const StateFun = () => {
   const [increaseState, increaseSetState] = useState(0);
   const [hideState, hideSetState] = useState(false);
+  const [inputState, inputSetState] = useState("");
+
+  const chageInput = (text) => {
+    inputSetState(text.target.value);
+  };
 
   return (
     <>
@@ -22,6 +27,13 @@ const StateFun = () => {
         >
           Показать/скрыть текст
         </button>
+		  </div>
+
+      <div>
+        <label>
+          <input type="text" value={inputState} onChange={chageInput} />
+        </label>
+        <p>text:{inputState}</p>
       </div>
     </>
   );
